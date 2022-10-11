88a2b12ae6d315f0a50c5be28c4ff5c471fe9885
Vikash Basant <basantvikash360@gmail.com>
2022-10-11 17:58:18 +0530

    Initial commit

---
+++ src/main/java/com/joblisting/JoblistingApplication.java
@@ -0,0 +1,15 @@
+package com.joblisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class JoblistingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoblistingApplication.class, args);
+	}
+
+}
